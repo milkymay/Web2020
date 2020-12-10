008de64fbe5ef6d2ef3fb156bf5bd8488ee817e6
milkymay <missisPatrik@yandex.ru>
2020-12-10 20:33:21 +0300

    lab10 initial commit + task1

---
+++ lab10/lesson10/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer>
+        <div> {{ usersCount }} users registered, {{ postsCount }} posts written </div>
+        <a href="/">Codeforces</a> &copy; 2020 by Mike Mirzayanov
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer",
+    props: ["usersCount", "postsCount"],
+}
+</script>
+
+<style scoped>
+
+</style>
